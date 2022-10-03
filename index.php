c102918596b8c149817e08ebc7abfc5de0773e72
MohdZairi <mohdzairi658@gmail.com>
2022-10-03 23:47:29 +0800

    change title and so on

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project Online Examination</title>
+    <title>Project Online Examination For Primary School</title>
 
     <link  rel="stylesheet" href="css/bootstrap.min.css"/>
     <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>    
